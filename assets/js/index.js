e9b515fa6121e781716d4f2ee582bfdad6fb5730
Luke Harris <luke@lkhrs.com>
2022-03-26 23:17:35 -0500

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+// Import the Bootstrap components we want to use.
+// See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
+
+// import Alert from 'js/bootstrap/src/alert'
+// import Button from 'js/bootstrap/src/button'
+// import Carousel from 'js/bootstrap/src/carousel'
+// import Collapse from 'js/bootstrap/src/collapse'
+// import Dropdown from 'js/bootstrap/src/dropdown'
+// import Modal from 'js/bootstrap/src/modal'
+// import Offcanvas from 'js/bootstrap/src/offcanvas'
+// import Popover from 'js/bootstrap/src/popover'
+// import ScrollSpy from 'js/bootstrap/src/scrollspy'
+// import Tab from 'js/bootstrap/src/tab'
+// import Toast from 'js/bootstrap/src/toast'
+// import Tooltip from 'js/bootstrap/src/tooltip'
+
+// Bootstrap toast popover code
+// (function () {
+//   let toastElList = [].slice.call(document.querySelectorAll(".toast"));
+//   let toastList = toastElList.map(function (toastEl) {
+//     return new Toast(toastEl);
+//   });
+
+//   toastList.forEach(function (toast) {
+//     toast.show();
+//   });
+
+//   let popoverTriggerList = [].slice.call(
+//     document.querySelectorAll('[data-bs-toggle="popover"]')
+//   );
+//   popoverTriggerList.map(function (popoverTriggerEl) {
+//     return new Popover(popoverTriggerEl);
+//   });
+// })();
+
+// Turbo import 
+import * as Turbo from '/jslibs/turbo/v7'
